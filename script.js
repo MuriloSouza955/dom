2834d8e7709d0c9e6671ba7301469f75dbebbf33
MuriloSouza955 <souza.murilo@gmail.com>
2025-04-27 19:43:22 -0300

    Inspecionando no Navegador e importando o template

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 /*
-DOM.
-O Document Object Model (DOM) é uma estrutura essencial manipulada com JavaScript para representar objetos em uma página web. O DOM é organizado em uma estrutura de árvore, onde elementos HTML são representados como nós e objetos. A aula destaca a relação entre os elementos HTML e a estrutura do DOM, mostrando como acessar e modificar esses elementos.
+Inspecionando no Navegador
+Neste vídeo, apresento uma aplicação simples em HTML e CSS, mostrando a estrutura do código. Demonstro como inspecionar elementos no navegador, destacando a hierarquia do DOM. Explico a relação entre os elementos e como manipulá-los com JavaScript.
 */
-
-console.log("Hello World!");
