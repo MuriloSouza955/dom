cd9738e72be4b4130148f2e5f4b5e5f6af2f1cd8
MuriloSouza955 <souza.murilo@gmail.com>
2025-04-27 19:36:48 -0300

    Iniciando repositório DOM

---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+DOM.
+O Document Object Model (DOM) é uma estrutura essencial manipulada com JavaScript para representar objetos em uma página web. O DOM é organizado em uma estrutura de árvore, onde elementos HTML são representados como nós e objetos. A aula destaca a relação entre os elementos HTML e a estrutura do DOM, mostrando como acessar e modificar esses elementos.
+*/
+
+console.log("Hello World!");
