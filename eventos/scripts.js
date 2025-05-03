c42018809be0e9569fc900704ed42122b7a807b7
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-02 22:34:31 -0300

    13 - Usando regex

--- eventos/scripts.js
+++ eventos/scripts.js
@@ -1,34 +1,34 @@
+const input = document.querySelector('input');
+const form = document.querySelector('form');
+
+
 /*
+input.addEventListener('input', (e) => {
+    const value = input.value;
+    
+    const regex = /\D+/g;
 
-Eventos em input
-Neste vídeo, expliquei sobre eventos de input em JavaScript. Demonstrei como capturar eventos de "keydown" e "keypress" em um campo de input, destacando as diferenças entre eles. Também mostrei o evento "change", que é acionado quando o conteúdo do input é alterado. Expliquei a importância de conhecer esses eventos para ampliar as possibilidades de programação em JavaScript e se tornar um especialista na linguagem. É fundamental entender essas estruturas para utilizar a melhor abordagem em cada situação.
+    //Retorna o padrão encontrado na string
+    // console.log(value.match(regex));
 
-
-
+    //Verifica se o padrão é atendido
+    // const isValue = regex.test(value);
+    // console.log(isValue);
+});
 */
 
-const input = document.querySelector('input');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const value = input.value;
+    const regex = /\D+/g;
 
-// //keydown - quando o usuário pressiona uma tecla (CTRL, SHIFT, TAB, etc)
-// input.addEventListener('keydown', (event) => {
-//     console.log(event.key);
-// });
+    if (regex.test(value)) {
+        console.log(value)
+    }else{
+        alert("Padrão não encontrado!");
+    }
 
-//keypress - quando o usuário pressiona uma tecla do tipo caractere (letras, números, etc)
-input.addEventListener('keypress', (event) => {
-    console.log(event.key);
+
+    // const value = input.value.replace(regex, "");
+    // console.log(value);
 });
-
-//change - quando o conteúdo do input é alterado, mas dispara somento quando sai a caixa de texto.
-input.onchange = () => {
-    console.log('O conteúdo do input foi alterado!');
-};
-
-input.onchange = () => {
-    inputChange();
-};
-
-
-function inputChange() {
-    console.log('O conteúdo do input foi alterado!');
-}
